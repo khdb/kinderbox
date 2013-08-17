e4cb08e8ab109a4e3e745cf942184f4e86596603
khdb <khoa.do.bui@gmail.com>
2013-08-17 05:28:32 +0000

    initial commit

---
+++ playground/relay-pi.py
@@ -0,0 +1,31 @@
+##17/08/2013
+##Khoa DO: testing GPIO with a relay board
+##This script will define a GPIO pin as ouput and switch the connect relay on and off
+##The loop count and delay/sleep time will be read from the command line interactively.
+
+
+import RPi.GPIO as GPIO ## Import GPIO library
+import time ## Import 'time' library. Allows us to use 'sleep'
+
+myPin = 4
+
+GPIO.setmode(GPIO.BCM) ## Use board pin numbering
+GPIO.setup(myPin, GPIO.OUT) ## Setup GPIO the Pin to OUT
+
+##Define a function named OnandOff()
+def OnandOff(numTimes,speed):
+        for i in range(0,numTimes):## Run loop numTimes
+                print "Iteration " + str(i+1)## Print current loop
+                GPIO.output(myPin,True)## Switch on pin 
+                time.sleep(speed)## Wait
+                GPIO.output(myPin,False)## Switch off pin 
+                time.sleep(speed)## Wait
+        print "Done" ## When loop is complete, print "Done"
+        GPIO.cleanup()
+
+## Ask user for total number of switches and length of each blink
+iterations = raw_input("Enter total number of times to switch: ")
+speed = raw_input("Enter length of each switch(seconds): ")
+
+## Start OnandOff() function. Convert user input from strings to numeric data types and pass to OnandOff() as parameters
+OnandOff(int(iterations),float(speed))
