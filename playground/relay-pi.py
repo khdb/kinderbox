ce5a63d7851f1466be2b06c8b93a874daa78c042
khdb <khoa.do.bui@gmail.com>
2013-08-19 09:59:45 +0000

    Add more pins

--- playground/relay-pi.py
+++ playground/relay-pi.py
@@ -8,17 +8,23 @@ import RPi.GPIO as GPIO ## Import GPIO library
 import time ## Import 'time' library. Allows us to use 'sleep'
 
 myPin = 4
+myPin2 = 17
 
 GPIO.setmode(GPIO.BCM) ## Use board pin numbering
 GPIO.setup(myPin, GPIO.OUT) ## Setup GPIO the Pin to OUT
+GPIO.setup(myPin2, GPIO.OUT)
 
 ##Define a function named OnandOff()
 def OnandOff(numTimes,speed):
         for i in range(0,numTimes):## Run loop numTimes
                 print "Iteration " + str(i+1)## Print current loop
-                GPIO.output(myPin,True)## Switch on pin 
+		if i%2==0:
+                	GPIO.output(myPin,True)## Switch on pin 
+		else:
+                	GPIO.output(myPin2,True)## Switch on pin 
                 time.sleep(speed)## Wait
                 GPIO.output(myPin,False)## Switch off pin 
+                GPIO.output(myPin2,False)## Switch off pin 
                 time.sleep(speed)## Wait
         print "Done" ## When loop is complete, print "Done"
         GPIO.cleanup()
